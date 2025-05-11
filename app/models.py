cf5149ac3bbda8325e8c4502df97d8996c73e609
Chris Harrel <harrelchris@gmail.com>
2025-05-11 11:11:15 -0500

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Sitemap(Base):
+    __tablename__ = "sitemap"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    url: Mapped[str] = mapped_column(String(100))
+    updated: Mapped[str] = mapped_column(String(20))
+    retrieved: Mapped[str] = mapped_column(String(20))
+
+    def __repr__(self) -> str:
+        return self.url
+
+
+class Decision(Base):
+    __tablename__ = "decision"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    url: Mapped[str] = mapped_column(String(100))
+    updated: Mapped[str] = mapped_column(String(20))
+    retrieved: Mapped[str] = mapped_column(String(20))
+
+    def __repr__(self) -> str:
+        return self.url
